263f88ba0bebff1cee08d2dda7343a4140e188b4
SpongGithub <80288631+SpongGithub@users.noreply.github.com>
2021-03-11 15:12:58 +0100

    Code.java

--- Code.java
+++ Code.java
@@ -1,160 +1,161 @@
-float spieler_x;
-float spieler_y;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.content.Context;
+import android.app.Activity;
+Activity act;
+Context cnt;
+SharedPreferences sp;
+SharedPreferences.Editor editor;
+//PImage st; //st = settings
+PImage skin_1;
+PImage char_1;
+int spieler_x;
+int spieler_y;
 int spieler_w;
-int spieler_h;
-
+int spieler_h; 
 float ball_x;
 float ball_y;
 float ball_ge_x;
 float ball_ge_y;
 int ball_d;
+int myScore;
+String myScoreFile;
+int seite = 0; //0 = start, 1 = play, 2 = end 
+void setup() { 
+  frameRate(120);
+  //st = loadImage("st.png");
+  skin_1 = loadImage("skin_1.png");
+  char_1 = loadImage("char_1.png");
 
-int score;
-
-int Hi_score;
-int seite = 0;  //0 = start , 1 = play , 2 = end
-
-void setup() {
-  
-  //am pc gibt es kein font ... einfach die // weg machen
-  //PFont font = createFont("SansSerif", 24 * displayDensity);
-  //textFont(font);
-
-
-  //einfach vor size // machen und vor fullscreen die // weg machen
-  size(400, 800);
-  //fullScreen();
-  rectMode(CENTER);
-  
-  
-  String[] data=loadStrings("lines.txt");
-  Hi_score = int(data[0]);
-
-  spieler_w = width/15;
-  spieler_h = height/5;
+  PFont font = createFont("SansSerif", 24 * displayDensity); 
+  textFont(font); 
+  fullScreen(); 
+  rectMode(CENTER); 
+  spieler_w = width/15; 
+  spieler_h = height/5; 
   ball_d = width/12;
-
-  reset();
-}
-
-void draw() {
+  act = this.getActivity();
+  cnt = act.getApplicationContext();
+  sp = PreferenceManager.getDefaultSharedPreferences(cnt);
+  editor = sp.edit();
+  myScoreFile = "Scores";
+  myScore = loadScore(myScoreFile);
+} 
+void draw() { 
   background(255);
+  //image(st, width/1.2, height/20);
+  //st.resize(width/8, height/16);
   if (seite == 0) {
-    showStart();
-
-    if (mousePressed) {
-      if (mouseX > width/2 - width/3/2 && mouseX < width/2 + width/3/2 && mouseY > height/1.5 - height/12/2 && mouseY < height/1.5 + height/12/2) {
-        reset();
+    showStart(); 
+    if (mousePressed) { 
+      if (mouseX > width/2 - width/3/2 && mouseX < width/2 + width/3/2 && mouseY > height/1.5 - height/12/2 && mouseY < height/1.5 + height/12/2) { 
+        reset(); 
         seite = 1;
       }
     }
-    
-  }
-
-  if (seite == 1) {
-    showGame();
-    berechnePlayerY();
-    
-    ball_x = ball_x + ball_ge_x;
-    ball_y = ball_y + ball_ge_y;
-    
+  } 
+  if (seite == 1) { 
+    showGame(); 
+    berechnePlayerY(); 
+    ball_x = ball_x + ball_ge_x; 
+    ball_y = ball_y + ball_ge_y; 
     berechneBall();
-    
-  }
-
-  if (seite == 2) {
-    showEnd();
-    
-    if (mousePressed) {
-      if (mouseX > width/2 - width/3/2 && mouseX < width/2 + width/3/2 && mouseY > height/1.5 - height/12/2 && mouseY < height/1.5 + height/12/2) {
+  } 
+  if (seite == 2) { 
+    showEnd(); 
+    if (mousePressed) { 
+      if (mouseX > width/2 - width/3/2 && mouseX < width/2 + width/3/2 && mouseY > height/1.5 - height/12/2 && mouseY < height/1.5 + height/12/2) { 
         reset();
-        seite = 1;
+        delay(300);
+        seite = 0;
       }
     }
   }
-  
-}
-
-
-void reset() {
-  score = 0;
-  spieler_x = width/10;
-  spieler_y = height/2;
-  ball_x = width/2;
-  ball_y = height/2;
-  ball_ge_x = -4;
+} 
+void reset() { 
+  spieler_x = width/10; 
+  spieler_y = height/2; 
+  ball_x = width/2; 
+  ball_y = height/2; 
+  ball_ge_x = -4; 
   ball_ge_y = 0;
-}
-
+} 
 void showGame() {
+  fill(0);
+  text(str(myScore), width/2, height/14);
   fill(255);
-  rect(spieler_x, spieler_y, spieler_w, spieler_h, 7);
-  ellipse(ball_x, ball_y, ball_d, ball_d);
-  line(0, height/2, width, height/2);
-  fill(0);
-  text(score + "/"+ Hi_score,width/2,height/10);
-}
-
-void showStart() {
-  fill(255);
-  rect(width/2, height/1.5, width/3, height/12, 7);
-  textAlign(CENTER);
-  textSize(40);
-  fill(0);
-  text("SPONG", width/2, height/5);
-  textSize(20);
-  fill(0);
+  image(char_1, spieler_x-spieler_w/2, spieler_y-spieler_h/2);
+  char_1.resize(spieler_w, spieler_h);
+  image(skin_1, ball_x, ball_y);
+  skin_1.resize(ball_d, ball_d);
+  //ellipse(ball_x, ball_y, ball_d, ball_d);
+} 
+void showStart() { 
+  fill(255); 
+  rect(width/2, height/1.5, width/3, height/12, 7); 
+  textAlign(CENTER); 
+  textSize(40); 
+  fill(0); 
+  text("SPONG", width/2, height/5); 
+  textSize(20); 
+  fill(0); 
   text("START", width/2, height/1.49);
 }
-
-void showEnd(){
-  fill(255);
-  rect(width/2, height/1.5, width/3, height/12, 7);
-  textAlign(CENTER);
-  textSize(40);
-  fill(0);
-  text("VERLOREN", width/2, height/5);
-  textSize(20);
-  fill(0);
+void showEnd() { 
+  fill(255); 
+  rect(width/2, height/1.5, width/3, height/12, 7); 
+  textAlign(CENTER); 
+  textSize(40); 
+  fill(0); 
+  text("VERLOREN", width/2, height/5); 
+  textSize(20); 
+  fill(0); 
   text("TRY AGAIN", width/2, height/1.49);
+} 
+void berechnePlayerY() { 
+  if (mousePressed) { 
+    if (mouseY > height / 2) { 
+      if (spieler_y < height - spieler_h/2) { 
+        spieler_y = spieler_y + 10;
+      }
+    } 
+    if (mouseY < height / 2) { 
+      if (spieler_y > spieler_h/2) { 
+        spieler_y = spieler_y - 10;
+      }
+    }
+  }
+} 
+void berechneBall() { 
+  if (ball_x < spieler_x + spieler_w) { 
+    if (ball_y < (spieler_y + spieler_h/2) && ball_y > (spieler_y - spieler_h/2)) { 
+      ball_ge_x = (-ball_ge_x) + 1 * 1; 
+      ball_ge_y = ball_ge_y - (spieler_y - ball_y) * 0.1; 
+      myScore++;
+      saveScore(myScore, myScoreFile);
+    } else {
+      seite = 2;
+    }
+  } 
+  if (ball_y > height-ball_d/2 || ball_y < ball_d/2) { 
+    ball_ge_y = -ball_ge_y;
+  } 
+  if (ball_x > width-ball_d/2) { 
+    ball_ge_x = -ball_ge_x;
+  }
+}
+void saveScore(int score, String name) {
+  //editor.clear();
+  editor.putInt(name, score);
+  editor.commit();
 }
 
-void berechnePlayerY(){
-  if (mousePressed) {
-      if (mouseY > height / 2) {
-        if (spieler_y < height - spieler_h/2) {
-          spieler_y = spieler_y + 10;
-        }
-      }
-      if (mouseY < height / 2) {
-        if (spieler_y > spieler_h/2) {     
-          spieler_y = spieler_y - 10;
-        }
-      }
-    }
+int loadScore(String name) {
+  int getScore = sp.getInt(name, 0);
+  return getScore;
 }
 
-void berechneBall(){
-  if (ball_x < spieler_x + spieler_w) {
-      if (ball_y < (spieler_y + spieler_h/2) && ball_y > (spieler_y - spieler_h/2)) {
-        ball_ge_x = (-ball_ge_x) + 0.4 * 1;
-        ball_ge_y = ball_ge_y - (spieler_y - ball_y) * 0.1;
-        score = score + 1;
-        if(score > Hi_score){
-          String[] data=loadStrings("lines.txt");
-          String[] newTxt = {str(int(data[data.length -1]) + 1)}; 
-          saveStrings("lines.txt", newTxt);
-          Hi_score ++;
-        }
-      } else {
-        seite = 2;
-      }
-    }
-    if (ball_y > height-ball_d/2 || ball_y < ball_d/2) {
-      ball_ge_y = -ball_ge_y;
-    }
-    if (ball_x > width-ball_d/2) { 
-      ball_ge_x = -ball_ge_x;
-    }
-  
+void onPause() {
+  super.onPause();
+  saveScore(myScore, myScoreFile);
 }
